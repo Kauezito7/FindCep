84f32560080ab63f6ab4700016551c55177a7131
Kaue Moura <Kauehago07@gmail.com>
2025-08-14 11:21:59 -0300

    feat: Finalizando card e tela

--- app/index.jsx
+++ app/index.jsx
@@ -1,33 +1,39 @@
 // import { ImageBackground } from "expo-image";
-import { Text, View, StyleSheet, ImageBackground, Image } from "react-native";
+import { Text, View, StyleSheet, ImageBackground, Image, ScrollView } from "react-native";
 import { Input } from '../components/input/input';
 import { Botao } from '../components/botao/botao';
+import { Card } from '../components/card/card';
+
+
 
 export default function Index() {
   return (
     <>
-    {/* 1. Logo + imagem de fundo */}
-    <ImageBackground 
-    source={require('../assets/images/ImgFundo.png')} 
-    style={styles.imgFundo}>
+      {/* 1. Logo + imagem de fundo */}
+      <ImageBackground
+        source={require('../assets/images/ImgFundo.png')}
+        style={styles.imgFundo}>
 
-    <Image
-    source={require('../assets/images/LogoFindCEP.png')} 
-    style={styles.loho}>
-    </Image>
+        <Image
+          source={require('../assets/images/LogoFindCEP.png')}
+          style={styles.loho}>
+        </Image>
 
-    </ImageBackground>
+      </ImageBackground>
 
-    {/* 2. Campo de consulta */}
-    <View style={styles.container}>
-    {/* 2.1 Titulo */}
-    <Text style={styles.titulo}>Consulte seu Cep</Text>
-    {/* 2.2 Input */}
-    <Input/>
-    <Botao tituloBotao='Consultar'/>
-    </View>
-    {/* 2.3 Botao */}
-    {/* 2.4 Card de informacoes */}
+      <ScrollView style={styles.containerScroll}>
+        {/* 2. Campo de consulta */}
+        <View style={styles.container}>
+          {/* 2.1 Titulo */}
+          <Text style={styles.titulo}>Consulte seu Cep</Text>
+          {/* 2.2 Input */}
+          <Input />
+          {/* 2.3 Botao */}
+          <Botao tituloBotao='Consultar' />
+          {/* 2.4 Card de informacoes */}
+          <Card card="Informacoes do CEP" />
+        </View>
+      </ScrollView>
     </>
   );
 }
@@ -41,16 +47,21 @@ const styles = StyleSheet.create({
     height: '100%'
   },
   Logo: {
-
     width: 100,
     height: 120
   },
   container: {
+    gap: 40,
+    width: "100%",
+    minHeight: '100%',
+    alignItems: 'center'
+  },
+  containerScroll: {
     flex: 1.5,
-    alignItems: 'center',
     paddingTop: 50,
-    paddingBottom: 50,
-    gap: 40
+    height: '100%',
+    paddingBottom: 80
+
   },
   titulo: {
     fontFamily: 'Poppins-Bold',
